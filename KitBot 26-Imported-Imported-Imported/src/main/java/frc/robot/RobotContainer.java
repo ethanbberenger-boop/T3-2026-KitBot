b3cc391f9651af3b4b2a72a9c86175468668d740
Ethan <ethanbberenger@gmail.com>
2026-02-14 17:26:09 -0600

    \

--- KitBot 26-Imported-Imported-Imported/src/main/java/frc/robot/RobotContainer.java
+++ KitBot 26-Imported-Imported-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -14,8 +14,6 @@ import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Input;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.TankDrive;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -48,7 +46,7 @@ public class RobotContainer {
     inputmove = new inputMove(shooter);
     outputmove = new outputMove(shooter);
     higher = new Higher(shooter);
-    SmartDashboard.putNumber("BatteryVolts", RobotController.getBatteryVoltage());
+    configureBindings();
       }
     
     
